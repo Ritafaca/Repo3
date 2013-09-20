9e912f1d7e68b52af39ac48e4ebf828d314625c8
unknown <muhamadalfian>
2013-09-20 16:32:50 +0700

    Praktikum3

---
+++ Praktikum3/Classpath/main.java
@@ -0,0 +1,8 @@
+import praktikum3.Classpath;
+public class main{
+	public static void main(String[] args){
+		Latihan l = new Latihan();
+		l.setNama("ini nama latihan");
+		System.out.println(l.getNama());
+	}
+}
